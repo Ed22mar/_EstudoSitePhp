de69d8e8d341281eca4c08546d7f80ef8ce4d574
Edgar Marcos <edgaricloud@gmail.com>
2022-04-07 18:59:12 +0100

    updated produto

--- produtos.php
+++ produtos.php
@@ -1,59 +1,54 @@
 <?php include("template/cabecario.php");?>
 
     <div class="col-md-3">
-
         <div class="card">
 
             <img class="card-img-top" src="holder.js/100x180/" alt="">
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
+                <h4 class="card-title">Livro PHP</h4>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mais</a>
             </div>
 
         </div>
-
     </div>  
 
+    
     <div class="col-md-3">
-
-            <div class="card">
-
-                <img class="card-img-top" src="holder.js/100x180/" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">Title</h4>
-                    <p class="card-text">Text</p>
-                </div>
-
-            </div>
-
-    </div>  
-
-    <div class="col-md-3">
-
         <div class="card">
 
             <img class="card-img-top" src="holder.js/100x180/" alt="">
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
+                <h4 class="card-title">Livro PHP</h4>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mais</a>
             </div>
 
         </div>
-
     </div>  
 
+    
     <div class="col-md-3">
-
         <div class="card">
 
             <img class="card-img-top" src="holder.js/100x180/" alt="">
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
+                <h4 class="card-title">Livro PHP</h4>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mais</a>
             </div>
 
         </div>
+    </div>  
 
+    
+    <div class="col-md-3">
+        <div class="card">
+
+            <img class="card-img-top" src="holder.js/100x180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Livro PHP</h4>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mais</a>
+            </div>
+
+        </div>
     </div>  
 
 
