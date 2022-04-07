1c8e9b538c197d6119fa9564c5531af7634e121f
Edgar Marcos <edgaricloud@gmail.com>
2022-04-07 23:33:59 +0100

    u

--- administrador/index.php
+++ administrador/index.php
@@ -4,10 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador do sitio web</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Administrador</title>
 </head>
 <body>
     
-    Login
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header bg-primary">
+                        Login
+                    </div>
+                    <div class="card-body">
+
+                      <div class="form-group">
+                          <label for="exampleInputEmail1">Usuário</label>
+                      </div>  
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
 </body>
 </html>
