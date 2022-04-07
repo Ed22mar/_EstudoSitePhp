dcc42d2504474e97a499164063783256803a1902
Edgar Marcos <edgaricloud@gmail.com>
2022-04-07 11:24:29 +0100

    index creation
    Implementação da estrutura html 5

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "ola Developer ";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site web</title>
+</head>
+<body>
+    Sitio web
+</body>
+</html>
