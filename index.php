9b0afca772a9d1c0ae57bd313ce8b7321a222a46
Edgar Marcos <edgaricloud@gmail.com>
2022-04-07 12:10:52 +0100

    Criação do cabeçario
    Implementação dos elementos que fazem parte do cabeçario da página

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<?php
-    
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site web</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
-<body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Develoteca </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Livros</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Nosotros</a>
-            </li>
-        </ul>
-    </nav>
-
-    <div class="container">
-        <div class="row">
 
             <div class="jumbotron">
                 <h1 class="display-3">Jumbo heading</h1>
@@ -42,8 +10,7 @@
                 </p>
             </div>
 
-            
-        </div>
+            </div>
     </div>
    
 </body>
