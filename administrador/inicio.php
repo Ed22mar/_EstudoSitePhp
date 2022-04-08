5f033bb952319756892c99c82c0ead85d579b8e2
Edgar Marcos <edgaricloud@gmail.com>
2022-04-08 15:01:25 +0100

    u

---
+++ administrador/inicio.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#">Administrador do Site <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Inicio</a>
+            <a class="nav-item nav-link" href="#">Livros</a>
+            <a class="nav-item nav-link" href="#">Terminar Secção</a>
+            <a class="nav-item nav-link" href="#">Ver sitio Web</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+                <div class="jumbotron">
+                    <h1 class="display-3">Jumbo heading</h1>
+                    <p class="lead">Jumbo helper text</p>
+                    <hr class="my-2">
+                    <p>More info</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                    </p>
+                </div>
+
+            </div>
+            
+        </div>
+    </div>
+      
+
+
+  </body>
+</html>
