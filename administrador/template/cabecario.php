5ae35f73e1d26f8853f4cfc7e2e64686a8f3bac2
Edgar Marcos <edgaricloud@gmail.com>
2022-04-08 15:02:37 +0100

    estrutura admin

---
+++ administrador/template/cabecario.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#">Administrador do Site <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Inicio</a>
+            <a class="nav-item nav-link" href="#">Livros</a>
+            <a class="nav-item nav-link" href="#">Terminar Secção</a>
+            <a class="nav-item nav-link" href="#">Ver sitio Web</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
