0011deb90a6607231763767b87df1a2f22f3ecdc
Edgar Marcos <edgaricloud@gmail.com>
2022-04-08 16:21:34 +0100

    u
    implementação do formulário de cadastros de livros

--- administrador/seccao/produtos.php
+++ administrador/seccao/produtos.php
@@ -1,3 +1,38 @@
 <?php include("../template/cabecario.php");?>
 
+<div class="col-md-5">
+
+    <div class="card">
+        <div class="card-header">
+            Dados do Livro
+        </div>
+        <div class="card-body">
+            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="txtID">ID:</label>
+                    <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+                </div>
+                <div class="form-group">
+                    <label for="txtNome">Nome:</label>
+                    <input type="text" class="form-control" name="txtNome" id="txtNome" placeholder="Nome do livro">
+                </div>
+                <div class="form-group">
+                    <label for="">Imagem:</label>
+                    <input type="file" class="form-control" name="" id="">
+                </div>
+                <div class="btn-group" role="group" aria-label="">
+                    <button type="button" class="btn btn-success">Salvar</button>
+                    <button type="button" class="btn btn-warning">Modificar</button>
+                    <button type="button" class="btn btn-info">cancelar</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    
+</div>
+<div class="col-md-7">
+    Tabela de livros(mostrar os dados dos livros)
+</div>
 <?php include("../template/rodape.php");?>
