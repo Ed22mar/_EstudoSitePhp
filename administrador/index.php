07a26b2db5fb3acfa1591265347587870be6e42d
Edgar Marcos <edgaricloud@gmail.com>
2022-04-08 12:08:00 +0100

    u

--- administrador/index.php
+++ administrador/index.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header bg-primary">
+                    <div class="card-header">
                         Login
                     </div>
                     <div class="card-body">
