e2464027019ad646b4a5c184fbe5850ae6ac7d3c
Edgar Marcos <edgaricloud@gmail.com>
2022-04-08 15:18:00 +0100

    u

--- administrador/template/cabecario.php
+++ administrador/template/cabecario.php
@@ -9,15 +9,21 @@
   </head>
   <body>
 
+    <?php 
+        //REDIR PARA A PAGINA INICIAL DO SISTEMA
+        $url="http://".$_SERVER['HTTP_HOST']."/_MeuGit/_EstudoSitePhp";
+    ?>
+
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <a class="nav-item nav-link active" href="#">Administrador do Site <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="#">Início</a>
             <a class="nav-item nav-link" href="#">Livros</a>
             <a class="nav-item nav-link" href="#">Terminar Secção</a>
-            <a class="nav-item nav-link" href="#">Ver sitio Web</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>">Ver sitio Web</a>
         </div>
     </nav>
 
     <div class="container">
+        <br/>
         <div class="row">
